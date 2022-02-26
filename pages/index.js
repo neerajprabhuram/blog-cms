78951b26215e76b3231a8889c168bb4998fb0f12
Neeraj <nxxwxj@gmail.com>
2022-02-26 15:54:16 +0530

    cleared up files and installed dependencies

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Blog CMS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
+
+export default Home
